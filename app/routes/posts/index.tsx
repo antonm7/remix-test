f41d5f8192aab3989765ebf8e1ed07c898cb0850
antonm7 <migolkoanton@gmail.com>
2022-01-29 12:15:05 +0200

    Testing something

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -9,6 +9,8 @@ export default function Posts() {
         <div>
             <h1>Index Route</h1>
             <Link to="new">Nea Feature</Link>
+            <Link to="new">World</Link>
+
             <Outlet />
         </div>
     )
